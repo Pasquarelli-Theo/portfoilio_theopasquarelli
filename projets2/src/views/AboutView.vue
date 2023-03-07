e6c18073a468a73a25754e4c878b8de85eec5dc5
Pasquarelli-Theo <theo.pasquarelli@edu.univ-fcomte.fr>
2023-03-07 17:48:08 +0100

    about part2

--- projets2/src/views/AboutView.vue
+++ projets2/src/views/AboutView.vue
@@ -12,5 +12,25 @@ import bouton from "@/components/bouton.vue";
     </div>
     <div class="relative z-20 -ml-28 flex justify-center"><img src="../assets/info.png" class="" /></div>
     <div class="absolute right-0 z-0 -mt-40"><img src="../assets/CroixProjets.png" class="" /></div>
+    <h4 class="ml-44 font-arsenica text-4xl text-white">Designer & Web Designer</h4>
+    <div class="flex justify-center">
+      <p class="w-9/12 font-okine mt-10 text-base text-white">
+        Bonjour ! Je m’appelle Théo Pasquarelli et je vous souhaite la bienvenue sur mon portfolio. Depuis toujours je suis passionné par la
+        création et principalement le dessin qui m’a occupé durant des années et m’occupe encore. Exercer un métier créatif était donc une
+        évidence pour moi. Mes domaines de prédilections sont le graphisme et le web design cependant tout type de création m’intéresse, la
+        vidéo, la musique et en général la plupart des pratiques artistiques. C’est avec envie et impatience que je me lance à présent dans
+        le monde du design et du web design, je suis ouvert à tout type de projet et ai hâte d’emmagasiner de l’expérience.
+      </p>
+    </div>
+    <h4 class="ml-44 mt-20 font-arsenica text-4xl text-white">Parcours et compétences</h4>
+    <div class="flex justify-center">
+      <p class="w-9/12 font-okine mt-10 text-base text-white">
+        Bonjour ! Je m’appelle Théo Pasquarelli et je vous souhaite la bienvenue sur mon portfolio. Depuis toujours je suis passionné par la
+        création et principalement le dessin qui m’a occupé durant des années et m’occupe encore. Exercer un métier créatif était donc une
+        évidence pour moi. Mes domaines de prédilections sont le graphisme et le web design cependant tout type de création m’intéresse, la
+        vidéo, la musique et en général la plupart des pratiques artistiques. C’est avec envie et impatience que je me lance à présent dans
+        le monde du design et du web design, je suis ouvert à tout type de projet et ai hâte d’emmagasiner de l’expérience.
+      </p>
+    </div>
   </main>
 </template>
