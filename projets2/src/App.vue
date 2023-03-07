3b6e3a15a56439c7b61169f7328ee0b9e42a9580
Pasquarelli-Theo <theo.pasquarelli@edu.univ-fcomte.fr>
2023-03-07 08:01:30 +0100

    footer

--- projets2/src/App.vue
+++ projets2/src/App.vue
@@ -10,12 +10,13 @@ import { RouterLink, RouterView } from "vue-router";
     <router-view />
   </Suspense>
   <footer class="bg-black text-white">
-    <br /><div class="grid grid-flow-row-dense grid-cols-2">
-    <img src="@/assets/logoBlanc.svg" class="m-8 w-36 bg-black" />
-    <div class="-ml-96 mt-10">
-      <p class="font-okine text-xs -ml-36">+33(0)6 62 92 47 75</p>
-      <a href="mailto:theopasquarelli68@gmail.com" class="-ml-36 font-okine text-xs">theopasquarelli68@gmail.com</a>
-    </div>
+    <br />
+    <div class="grid grid-flow-row-dense grid-cols-2">
+      <img src="@/assets/logoBlanc.svg" class="m-8 w-36 bg-black" />
+      <div class="-ml-96 mt-10">
+        <p class="-ml-36 font-okine text-xs">+33(0)6 62 92 47 75</p>
+        <a href="mailto:theopasquarelli68@gmail.com" class="-ml-36 font-okine text-xs">theopasquarelli68@gmail.com</a>
+      </div>
     </div>
 
     <div class="absolute right-0 -mt-28 mr-28 flex bg-black">
