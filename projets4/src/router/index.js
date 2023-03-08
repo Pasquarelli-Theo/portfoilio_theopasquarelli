98bc669a5f8e0ec2eb2c04f12fe046d4385ab9b8
Pasquarelli-Theo <theo.pasquarelli@edu.univ-fcomte.fr>
2023-03-08 20:13:19 +0100

    crea osaka bpm PC

--- projets4/src/router/index.js
+++ projets4/src/router/index.js
@@ -5,6 +5,9 @@ import AboutView from "../views/AboutView.vue";
 import SkyView from "../views/SkyView.vue";
 import MentionsView from "../views/MentionsView.vue";
 import ProjetsView from "../views/ProjetsView.vue";
+import PetitCorsaireView from "../views/PetitCorsaireView.vue";
+import OsakaView from "../views/OsakaView.vue";
+import bpmView from "../views/bpmView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -12,12 +15,14 @@ const router = createRouter({
     { path: "/", name: "home", component: HomeView },
     { path: "/contact", name: "contact", component: ContactView },
     { path: "/about", name: "about", component: AboutView },
-    { path: "/sky", name: "sky", component: SkyView },
+    { path: "/skywalker", name: "skywalker", component: SkyView },
     { path: "/mentions", name: "mentions", component: MentionsView },
     { path: "/projets", name: "projets", component: ProjetsView },
+    { path: "/petitcorsaire", name: "petitcorsaire", component: PetitCorsaireView },
+    { path: "/osaka2025", name: "osaka2025", component: OsakaView },
+    { path: "/13bpm", name: "13bpm", component: bpmView },
   ],
 });
 
 export default router;
-
 
--- projets4/src/views/ProjetsView.vue
+++ projets4/src/views/ProjetsView.vue
@@ -6,50 +6,51 @@ import bouton from "@/components/bouton.vue";
   <main class="bg-dark_grey1">
     <h2 class="absolute right-0 mt-14 mr-36 font-aesthetic text-6xl text-white">PROJETS</h2>
     <br />
-    <div class="absolute z-20 flex">
+    <RouterLink to="/skywalker" class=""
+          ><div class="absolute z-20 flex">
       <img src="../assets/skyP.png" class="mt-40 w-1/2" />
       <div class="mt-72">
-        <RouterLink to="/sky" class=""
-          ><p class="font-aesthetic text-3xl text-white hover:text-grey2">SkyWalker</p>
-          <p class="font-okine text-base font-light text-white">Site de sneakers personnalisables</p></RouterLink
-        >
+        <p class="font-aesthetic text-3xl text-white hover:text-grey2">SkyWalker</p>
+          <p class="font-okine text-base font-light text-white">Site de sneakers personnalisables</p>
       </div>
-    </div>
+    </div></RouterLink
+        >
 
     <div class="z-0 mt-80"><img src="../assets/Croix2.png" class="" /></div>
 
-    <div class="absolute z-20 -mt-44 mr-36 -ml-10 flex">
+    <RouterLink to="/13bpm" class=""
+          ><div class="absolute z-20 -mt-44 mr-36 -ml-10 flex">
       <div class="ml-96 mt-24 pl-32 text-right">
-        <RouterLink to="/sky" class=""
-          ><p class="font-aesthetic text-3xl text-white hover:text-grey2">13 BPM</p>
-          <p class="w-64 font-okine text-base font-light text-white">Site d’un festival de musique rap</p></RouterLink
-        >
+          <p class="font-aesthetic text-3xl text-white hover:text-grey2">13 BPM</p>
+          <p class="w-64 font-okine text-base font-light text-white">Site d’un festival de musique rap</p>
       </div>
       <img src="../assets/13bpmP.png" class="right-0 w-1/2" />
     </div>
+    </RouterLink>
 
-    <div class="flex mb-96 absolute z-20">
+    <RouterLink to="/osaka2025" class=""
+          ><div class="flex mb-96 absolute z-20">
       <img src="../assets/OsakaP.png" class="mt-40 ml-20 w-1/2" />
       <div class="mt-72 -ml-20">
-        <RouterLink to="/sky" class=""
-          ><p class="font-aesthetic text-3xl text-white hover:text-grey2">OSAKA 2025</p>
-          <p class="w-72 font-okine text-base font-light text-white">Affiche pour l’exposition universelle d’Osaka 2025</p></RouterLink
-        >
+          <p class="font-aesthetic text-3xl text-white hover:text-grey2">OSAKA 2025</p>
+          <p class="w-72 font-okine text-base font-light text-white">Affiche pour l’exposition universelle d’Osaka 2025</p>
       </div>
-    </div>
+    </div></RouterLink>
 
     <div class="absolute right-0 z-0 pt-52 mt-96"><img src="../assets/Croix2.png" class="-mt-52" /></div>
 
-    <div class="absolute z-20 flex text-right mt-96 pt-24">
+<RouterLink to="/petitcorsaire" class="">
+      <div class="absolute z-20 flex text-right mt-96 pt-24">
       <div class="mt-52 ml-96 ">
-        <RouterLink to="/sky" class=""
-          ><p class="font-aesthetic text-3xl w-96 text-white hover:text-grey2">PETIT CORSAIRE</p>
-          <p class="font-okine text-base font-light text-white">Illustration dans le style de Cassandre</p></RouterLink
-        >
+          <p class="font-aesthetic text-3xl w-96 text-white hover:text-grey2">PETIT CORSAIRE</p>
+          <p class="font-okine text-base font-light text-white">Illustration dans le style de Cassandre</p>
       </div>
       <img src="../assets/corsaireP.png" class="right-0 mt-20 w-1/2" />
     </div>
-    <div class="mb-96"><br></div><br>
+  
+  </RouterLink>
+  
+  <div class="mb-96"><br></div><br>
     <div class="mb-96"><br></div><br>
     <br><br>
   </main>
