9a5ba0e8c9b2bafb1af5c3066145cfddaefb74aa
Pasquarelli-Theo <theo.pasquarelli@edu.univ-fcomte.fr>
2023-03-09 19:40:31 +0100

    menu qui marche

--- projets4/src/App.vue
+++ projets4/src/App.vue
@@ -1,92 +1,68 @@
 <script setup>
 import { RouterLink, RouterView } from "vue-router";
-
+import { ref } from "@vue/reactivity";
 </script>
 
 <template>
-<header class="sticky top-0 z-40 h-16 w-full bg-white">
-      <div class="flex h-full w-full items-center justify-between">
-        <div class="w-40">
-          <RouterLink to="/">
-            <img
-              src="@/assets/logoNoir.svg"
-              alt="Logo"
-              class=""
-            />
-          </RouterLink>
-        </div>
-        
-        <div class="">
-          <MenuIcon
-            class="text-black w-10 cursor-pointer"
-            @click="MenuOpen = !MenuOpen"
-          />
-        </div>
-
-        <nav
-          class="absolute   h-screen w-full -translate-y-[200%] bg-red-100 text-lg"
-          :class="MenuOpen && 'translate-y-0'"
-        >
-          <XIcon
-            class="float-right  w-10"
-            @click="MenuOpen = !MenuOpen"
-          />
-          <ul
-            class=" flex flex-col gap-8 py-4 font-archivo-black uppercase text-black"
-          >
-            <RouterLink to="/" class="transition-colors duration-150 hover:text-red-100" @click="MenuOpen = !MenuOpen"
-              >1. HOME</RouterLink
-            >
-            <RouterLink to="/projets" @click="MenuOpen = !MenuOpen"
-              >2. PROJETS</RouterLink
-            >
-            <RouterLink to="/about" @click="MenuOpen = !MenuOpen"
-              >3. À PROPOS</RouterLink
-            >
-            <RouterLink to="/contact" @click="MenuOpen = !MenuOpen"
-              >4. CONTACT</RouterLink
-            >
-          </ul>
-        </nav>
+  <header class="sticky top-0 z-40 mt-10 h-16 w-full bg-white">
+    <div class="flex h-full w-full items-center justify-between">
+      <div class="w-40">
+        <RouterLink to="/">
+          <img src="@/assets/logoNoir.svg" alt="Logo" class="" />
+        </RouterLink>
       </div>
-    </header>
-  <!-- <header class="bg-white">
-    <RouterLink to="/"><img src="@/assets/logoNoir.svg" class="mt-5 ml-5 w-36" /></RouterLink>
 
-    <div class="absolute z-40"><div
-      class="bg-main-blue fixed h-screen w-screen overflow-y-hidden"
-      v-on:click="menuVisible = !menuVisible"
-      :class="{ hidden: menuVisible }"
-    >
-      <div class="mt-4 flex justify-start">
-        <button v-if="!menuVisible" v-on:click="menuVisible = !menuVisible" class="mx-4">
-          <img src="@/assets/menu_fermer.svg" alt="Menu Fermer" />
+      <!--Bouton pour afficher menu-->
+      <button
+        class="w-fi relative p-1"
+        aria-controls="menu"
+        :aria-expanded="menuOuvert"
+        @click="menuOuvert = !menuOuvert"
+        :class="[menuOuvert ? 'hidden' : '']"
+      >
+        <img src="@/assets/Menu.svg" class="" />
+        <span class="sr-only">Menu</span>
+      </button>
+
+      <!--Bouton pour cacher menu-->
+      <div @click="menuOuvert = !menuOuvert" :class="[menuOuvert ? '' : 'hidden']" class="m-5">
+        <button class="fixed z-[99]" aria-controls="menu" :aria-expanded="menuOuvert">
+          <img src="@/assets/menu_fermer.svg" class="h-8 w-8" />
+          <span class="sr-only">Menu</span>
         </button>
-      </div> -->
+      </div>
 
-
-      <!-- <div class="flex justify-center">
-        <div class="ml-8 mt-8 font-aesthetic flex flex-col text-left text-2xl">
-          <router-link class="my-6 text-white" to="/" :class="{ 'text-main-beige': $route.name === 'home' }"
-            >1. HOME
-          </router-link>
-
-          <router-link class="my-6 text-white" to="/projets" :class="{ 'text-main-beige': $route.name === 'galerie' }"
-            >2. PROJETS
-          </router-link>
-
-          <router-link class="my-6 text-white" to="/about" :class="{ 'text-main-beige': $route.name === 'classement' }"
-            >3. À PROPOS
-          </router-link>
-
-          <router-link class="my-6 text-white" to="/contact" :class="{ 'text-main-beige': $route.name === 'who' }"
-            >4. CONTACT
+      <!--Menu hamburger déroulé-->
+      <div
+        class="fixed inset-0 z-[90] translate-x-full motion-safe:transition-transform motion-safe:duration-1000"
+        :class="[' bg-light_grey1  text-purple', menuOuvert ? 'translate-x-0' : '']"
+      >
+        <div class="m-5 flex items-center">
+          <router-link to="/">
+            <logo class="stroke-purtext-purple stroke-2" />
           </router-link>
         </div>
-      </div> -->
-    <!-- </div>
+        <div class="mt-20 flex items-center justify-center">
+          <ul @click="menuOuvert = !menuOuvert">
+            <div class="items-between flex flex-col">
+              <li class="mt-5 font-aesthetic text-base text-purple">
+                <router-link class="" to="/histoire"> HISTOIRE </router-link>
+              </li>
+              <li class="mt-5 font-aesthetic text-base text-purple">
+                <router-link class="" to="/crea"> PÔLE CRÉA </router-link>
+              </li>
+              <li class="mt-5 font-aesthetic text-base text-purple">
+                <router-link class="" to="/event"> EVENT </router-link>
+              </li>
+              <li class="mt-5 font-aesthetic text-base text-purple">
+                <router-link class="" to="/event"> EVENT </router-link>
+              </li>
+            </div>
+          </ul>
+        </div>
+      </div>
     </div>
-  </header> -->
+  </header>
 
   <Suspense>
     <router-view />
@@ -118,10 +94,11 @@ export default {
     MenuIcon,
     XIcon,
   },
-  }
+};
+
+let menuOuvert = ref();
+menuOuvert.value = false;
 </script>
 
 <style>
-/* Import Styles application */
-@import "./assets/style.css";
 </style>
